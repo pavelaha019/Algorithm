5eba71b4bbbbb587eab0e2371823c90b6da2b5eb
Pavel Ahamed <121998324+pavelaha019@users.noreply.github.com>
2023-10-11 23:13:19 +0600

    Bubble Sort.cpp

--- Algorithm/Bubble Sort.cpp
+++ Algorithm/Bubble Sort.cpp
@@ -2,46 +2,38 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-void BubbleSort(int ar[], int n)
-{
-   
-    for(int i=0;i<n-1;i++)
-      {
-         int flag = 0;
-        for(int j=i+1;j<n-i-1;j++)
-        {
-           
-            if(ar[j]<ar[j+1])
-            {
-                  swap(ar[j],ar[j+1]);
-                  flag =1;
-            }
-        }
-      
-      if (flag == 0)
-      {
-        break;
-      }
-      
-    }
-}
+ void bubbleSort(int ar[], int n)
+ {
+     for(int i=0;i<n-1;i++)
+    {
+         for(int j=0;j<n-i-1;j++)
+         {
+             if(ar[j]>ar[j+i])
+             {
+                int temp=ar[j];
+                 ar[j]=ar[j+1];
+                 ar[j+1]=temp;
+             }
+         }
+     }
+ }
 
-int main()
-{
-    int n;
-    cout<<"Enter the size of the array: ";
-    cin>>n;
-    int ar[n];
-    cout<<"Enter the elements of the array: ";
-    for(int i=0;i<n;i++)
-    {
-        cin>>ar[i];
-    }
-    BubbleSort(ar, n);
-    cout<<"Sorted array: ";
-    for(int i=0;i<n;i++)
-    {
-        cout<<ar[i]<<" ";
-    }
-    return 0;
-}
+ int main()
+ {
+     int n;
+     cout<<"Enter the size of the array: ";
+     cin>>n;
+     int ar[n];
+     cout<<"Enter the elements of the array: ";
+     for(int i=0;i<n;i++)
+     {
+         cin>>ar[i];
+     }
+     bubbleSort(ar, n);
+     cout<<"Sorted array: ";
+     for(int i=0;i<n;i++)
+     {
+         cout<<ar[i]<<" ";
+     }
+     return 0;
+ }
