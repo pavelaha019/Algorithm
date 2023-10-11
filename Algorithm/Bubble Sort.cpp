8adf43708bd6a114c3546a2eceffe3ed9c59c78d
Pavel Ahamed <121998324+pavelaha019@users.noreply.github.com>
2023-10-11 23:37:13 +0600

    Bubble Sort.cpp

--- Algorithm/Bubble Sort.cpp
+++ Algorithm/Bubble Sort.cpp
@@ -1,39 +1,35 @@
-//Bubble Sort
 #include<bits/stdc++.h>
-using namespace std;
+ using namespace std;
 
- void bubbleSort(int ar[], int n)
- {
-     for(int i=0;i<n-1;i++)
-    {
-         for(int j=0;j<n-i-1;j++)
-         {
-             if(ar[j]>ar[j+i])
-             {
-                int temp=ar[j];
-                 ar[j]=ar[j+1];
-                 ar[j+1]=temp;
-             }
-         }
-     }
- }
+ void bubbleSort(int arr[], int n){
+   
+    for(int i=0; i<n; i++){
+        for(int j=0; j<n-i-1; j++){
+            if(arr[j]>arr[j+1]){
+                swap(arr[j],arr[j+1]);
+            }
+        }
+    }
+ 
+}
+
 
  int main()
  {
      int n;
      cout<<"Enter the size of the array: ";
      cin>>n;
-     int ar[n];
+     int arr[n];
      cout<<"Enter the elements of the array: ";
      for(int i=0;i<n;i++)
      {
-         cin>>ar[i];
+         cin>>arr[i];
      }
-     bubbleSort(ar, n);
+     bubbleSort(arr, n);
      cout<<"Sorted array: ";
      for(int i=0;i<n;i++)
      {
-         cout<<ar[i]<<" ";
+         cout<<arr[i]<<" ";
      }
      return 0;
  }
